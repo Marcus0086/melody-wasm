103d7db1cd803b38499977b4ec328bafbdf5797b
Marcus0086 <marcgupta456@gmail.com>
2022-02-19 23:52:35 +0530

    new melody according to main repo

---
+++ regex_bundle_rust.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} source
+* @returns {string}
+*/
+export function compile_to_regex(source: string): string;
---
+++ regex_bundle_rust.js
@@ -0,0 +1,2 @@
+import * as wasm from "./regex_bundle_rust_bg.wasm";
+export * from "./regex_bundle_rust_bg.js";
